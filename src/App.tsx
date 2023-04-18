03edde25c7cd2f85f3f283ad34bc119bfa21edd3
Al Zee <z@alz.ee>
2023-04-18 18:45:42 +0800

    interface DONE

--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,30 @@ import "./App.css";
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
+  const [digit, setDigit] = useState("");
+  const [digitMsg, setDigitMsg] = useState("");
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     setGreetMsg(await invoke("greet", { name }));
+    setDigitMsg('长度为 ' + digit);
   }
 
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
+      <h1>Namey</h1>
 
       <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
         <a href="https://tauri.app" target="_blank">
           <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
 
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+      <h4>修改文件名长度</h4>
+      <ul className="tip">
+      <li>删除文件名中的前缀 0 ，使文件名符合指定长度</li>
+      <li>或用 0 前置填充文件名至指定长度</li>
+      </ul>
 
       <div className="row">
         <form
@@ -39,13 +40,26 @@ function App() {
         >
           <input
             id="greet-input"
+            type="file"
+            webkitdirectory="true"
             onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
+            placeholder="选择目录"
           />
-          <button type="submit">Greet</button>
+          <div className="row2">
+          <input
+            id="digit"
+            type="number"
+            min="2"
+            required
+            onChange={(e) => setDigit(e.currentTarget.value)}
+            placeholder="文件名长度"
+          />
+          <button type="submit">确定</button>
+          </div>
         </form>
       </div>
       <p>{greetMsg}</p>
+      <p>{digitMsg}</p>
     </div>
   );
 }
