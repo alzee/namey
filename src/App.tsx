1c65c47f40ce45d3856b352367f76d929534cf69
Al Zee <z@alz.ee>
2023-04-19 11:55:22 +0800

    almost DONE

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,53 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
+import { open } from '@tauri-apps/api/dialog';
+import { renameFile, readDir } from '@tauri-apps/api/fs';
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
   const [digit, setDigit] = useState("");
-  const [digitMsg, setDigitMsg] = useState("");
+  const [msg, setMsg] = useState("");
+  const [dir, setDir] = useState("");
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-    setDigitMsg('长度为 ' + digit);
+  async function getDir() {
+    const dir = await open({directory: true})
+    setDir(dir)
+  }
+
+  async function rename(dir, digit) {
+    if (dir === null || dir === '') {
+      setMsg('请选择目录')
+    } else {
+      // process
+      const entries = await readDir(dir)
+      for (const entry of entries) {
+        // ignore dirs
+        if (entry.children === undefined) {
+          const arr = entry.name.split('.')
+          const ext = arr.pop()
+          // remove leadding 0s so don't need to worry about name.length gt or lt digit
+          const name = arr.join('.').replace(/^0+/, '')
+          const newName = name.padStart(digit, 0)
+          await renameFile(entry.path, dir + '/' + newName + '.' + ext);
+        }
+      }
+    }
+  }
+
+  function sendMsg(s: int) {
+    if (s === 0) {
+      setMsg('处理中...')
+    } else {
+      setMsg('完成')
+    }
+  }
+
+  async function go() {
+    sendMsg(0)
+    await rename(dir, digit);
+    sendMsg(1)
   }
 
   return (
@@ -27,15 +62,15 @@ function App() {
 
       <h4>修改文件名长度</h4>
       <ul className="tip">
-      <li>删除文件名中的前缀 0 ，使文件名符合指定长度</li>
-      <li>或用 0 前置填充文件名至指定长度</li>
+      <li>当文件名大于指定长度时，删除文件名中多余的前缀 0 </li>
+      <li>当文件名小于指定长度时，用 0 前置填充文件名</li>
       </ul>
 
       <div className="row">
         <form
           onSubmit={(e) => {
             e.preventDefault();
-            greet();
+            go()
           }}
         >
           <input
@@ -43,6 +78,13 @@ function App() {
             type="file"
             onChange={(e) => setName(e.currentTarget.value)}
             placeholder="选择目录"
+          />
+          <input
+            onClick={() => getDir()}
+            readOnly
+            required
+            placeholder="选择目录"
+            value={dir}
           />
           <div className="row2">
           <input
@@ -57,8 +99,7 @@ function App() {
           </div>
         </form>
       </div>
-      <p>{greetMsg}</p>
-      <p>{digitMsg}</p>
+      <p>{msg}</p>
     </div>
   );
 }
